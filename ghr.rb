6c1f1d0ba0521aa4d7fcda21e94cf77ab7547b31
sona-tar <sona.zip@gmail.com>
2015-06-05 23:22:49 +0000

    Refacotr condition style. OSX amd64 url and sha1 as default value.

--- ghr.rb
+++ ghr.rb
@@ -3,12 +3,11 @@ require "formula"
 class Ghr < Formula
   homepage "https://github.com/tcnksm/ghr"
   version 'v0.4.0'
-  if OS.mac?
-    if Hardware.is_64_bit?
-      url "https://github.com/tcnksm/ghr/releases/download/v0.4.0/ghr_v0.4.0_darwin_amd64.zip"
-      sha1 "1aa90dde58c3b15dfd1e2f90cbaa317ee4752855"
-    end
-  elsif OS.linux?
+
+  url "https://github.com/tcnksm/ghr/releases/download/v0.4.0/ghr_v0.4.0_darwin_amd64.zip"
+  sha1 "1aa90dde58c3b15dfd1e2f90cbaa317ee4752855"
+
+  if OS.linux?
     if Hardware.is_64_bit?
       url "https://github.com/tcnksm/ghr/releases/download/v0.4.0/ghr_v0.4.0_linux_amd64.zip"
       sha1 '0143c54913e3b51e8658e69ab70d01c5ced2e517'
