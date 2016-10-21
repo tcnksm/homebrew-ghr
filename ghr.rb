827bd50eb10e4d7f8703d4b508d73a89d6ea74e0
tcnksm <nsd22843@gmail.com>
2016-10-21 15:48:27 +0900

    0.5.2

--- ghr.rb
+++ ghr.rb
@@ -2,10 +2,10 @@ require "formula"
 
 class Ghr < Formula
   homepage "https://github.com/tcnksm/ghr"
-  version 'v0.5.1'
+  version 'v0.5.2'
 
-  url "https://github.com/tcnksm/ghr/releases/download/v0.5.1/ghr_v0.5.1_darwin_amd64.zip"
-  sha256 "aaf99f6aea4ef5af05bc569aa5d1d41e683dafd27e55ddb9f9af2607db7b8983"
+  url "https://github.com/tcnksm/ghr/releases/download/v0.5.2/ghr_v0.5.2_darwin_amd64.zip"
+  sha256 "7c36e59ccb9b9ba08b9fa4239a803c28d9585f38f4db05c7f223eefe625111b9"
 
   def install
     bin.install 'ghr'
