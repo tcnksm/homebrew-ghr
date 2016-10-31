780d9f4afc8efdce78ebc73ca6a960c5e0e09dab
tcnksm <nsd22843@gmail.com>
2016-10-31 21:29:48 +0900

    Fix

--- ghr.rb
+++ ghr.rb
@@ -4,7 +4,7 @@ class Ghr < Formula
   homepage "https://github.com/tcnksm/ghr"
   version 'v0.5.3'
 
-  url "https://github.com/tcnksm/ghr/releases/download/v0.5.3/ghr_v0.5.3darwin_amd64.zip"
+  url "https://github.com/tcnksm/ghr/releases/download/v0.5.3/ghr_v0.5.3_darwin_amd64.zip"
   sha256 "07efc0c0fea9c056f816b5d30e009601375f982c6c55962dc6c9cc36bc76fd06"
 
   def install
